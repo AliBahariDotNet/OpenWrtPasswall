8200b39f14dece26040a6ec60cd10d6571a47e3b
Ali Bahari <AliBahariDotNet@GMail.com>
2025-06-29 16:13:48 +0000

    Remove sensitive root password and unnecessary URL from setup script

--- XiaomMiRouter4AGigabitEditionSetup.sh
+++ XiaomMiRouter4AGigabitEditionSetup.sh
@@ -1,5 +1,3 @@
-## root password: .dWFW>8'IXgm-~N4UDL6}m|q
-
 uci set system.@system[0].zonename='Asia/Tehran'
 uci set system.@system[0].timezone='<+0330>-3:30'
 
@@ -22,4 +20,3 @@ rm -f install-passwall.sh && wget https://raw.githubusercontent.com/AliBahariDot
 ## https://archive.openwrt.org/releases/22.03.7/targets/ramips/mt7621/openwrt-22.03.7-ramips-mt7621-xiaomi_mi-router-4a-gigabit-squashfs-sysupgrade.bin
 ## https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-22.03/mipsel_24kc/passwall_packages/sing-box_1.11.14-1_mipsel_24kc.ipk
 ## https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-22.03/mipsel_24kc/passwall_packages/xray-core_25.6.8-1_mipsel_24kc.ipk
-## https://hdsub.asemanmokamel.ir/kmzA2Uibmhm6s/5bdcc189-5f28-46f4-850b-0b1367cd83b1/#%D8%AE%D8%A7%D9%86%D9%88%D8%A7%D8%AF%D9%87
