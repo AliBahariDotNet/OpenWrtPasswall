b27247b2defdc68d5af357627a9b352d6b7b4cd0
Ali Bahari <AliBahariDotNet@GMail.com>
2025-07-06 11:49:45 +0330

    Update XiaomMiRouter4AGigabitEditionSetup.sh

--- XiaomMiRouter4AGigabitEditionSetup.sh
+++ XiaomMiRouter4AGigabitEditionSetup.sh
@@ -3,15 +3,21 @@ uci set system.@system[0].timezone='<+0330>-3:30'
 
 uci set network.lan.ipaddr='192.168.2.1'
 
+uci set network.wan.dns='1.1.1.1 8.8.8.8'
+uci set network.wan6.dns='2606:4700:4700::1111 2001:4860:4860::8888'
+
 uci set wireless.radio1.cell_density='0'
 uci set wireless.default_radio1.ssid='OpenWrt-5G'
 uci set wireless.default_radio1.encryption='sae-mixed'
 uci set wireless.default_radio1.key='@ALI1363@'
 uci set wireless.default_radio1.ocv='0'
+uci set wireless.radio1.disabled='0'
 
 uci commit
 
 /sbin/reload_config
+
+/etc/init.d/network restart
 
 opkg update && opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade
 
