4e16e0d0cf91d3369c489dc5f9bf11c9ae9cdbd8
Ali Bahari <AliBahariDotNet@GMail.com>
2026-02-17 13:41:23 +0330

    Update install-xray-core.sh

--- install-xray-core.sh
+++ install-xray-core.sh
@@ -8,7 +8,7 @@ rm -f xray-core_26.2.6-1_mipsel_24kc.ipk
 
 wget https://github.com/AliBahariDotNet/OpenWrtPasswall/raw/refs/heads/github-repo/releases/packages-22.03/mipsel_24kc/passwall_packages/xray-core_26.2.6-1_mipsel_24kc.ipk
 
-opkg xray-core_26.2.6-1_mipsel_24kc.ipk -d ram
+opkg install xray-core_26.2.6-1_mipsel_24kc.ipk -d ram
 
 rm -f xray-core_26.2.6-1_mipsel_24kc.ipk
 
